95bec16548dffd5a3e4e3ab2be2aa58af8be3dbc
Femke Versweyveld <versweyveldfemke@gmail.com>
2025-08-03 19:10:18 +0200

    [Refactoring] Refactor & finetune

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -17,6 +17,9 @@ export class GildedRose {
     this.items = items;
   }
 
+  private readonly UPPER_QUALITY_BOUND = 50;
+  private readonly LOWER_QUALITY_BOUND = 0;
+
   updateQuality() {
     /** Throw error if input invalid */
     if (!this.items || !Array.isArray(this.items)) {
@@ -26,71 +29,68 @@ export class GildedRose {
     this.items
       .filter(item => !item.name.includes('Sulfuras')) // Sulfuras does not change quality or sellIn
       .forEach(item => {
+        /** Throw error if quality or sellIn is NaN */
+        if (typeof item.quality !== 'number' || typeof item.sellIn !== 'number') {
+          throw new Error(`Quality or sellIn is NaN`);
+        }
         /** Throw error if quality is out of bounds */
-        if (item.quality < 0) {
-          throw new Error('Quality cannot be negative');
-        }
-        if (item.quality > 50) {
-          throw new Error('Quality cannot exceed 50');
+        if (item.quality < this.LOWER_QUALITY_BOUND || item.quality > this.UPPER_QUALITY_BOUND) {
+          throw new Error(`Quality is out of bounds: ${item.quality}`);
         }
 
-        /** Different operations based on product type, with the default/standard behavior on the bottom */
 
-        switch (true) {
-          case item.name.includes('Aged Brie'):
-            this.updateAgedBrieQuality(item);
-            break;
-          case item.name.includes('Backstage passes'):
-            this.updateBackstagePassesQuality(item);
-            break;
-          case item.name.includes('Conjured'):
-            this.updateDefaultQuality(item, 2); // Conjured items degrade in quality twice as fast
-            break;
-          default:
-            this.updateDefaultQuality(item);
-            break;
+        if (item.name.includes('Aged Brie')) {
+          this.updateAgedBrieQuality(item);
         }
-        item.sellIn = item.sellIn - 1;
+        else if (item.name.includes('Backstage passes')) {
+          this.updateBackstagePassesQuality(item);
+        }
+        else if (item.name.includes('Conjured')) {
+          this.updateDefaultQuality(item, 2); // Conjured items degrade in quality twice as fast
+        }
+        else {
+          this.updateDefaultQuality(item);
+        }
+        item.sellIn--;
       });
 
     return this.items;
   }
 
-  updateAgedBrieQuality(item: Item) {
+  private updateAgedBrieQuality(item: Item) {
     if (item.sellIn > 0) {
-      item.quality += 1;
+      item.quality++;
     } else {
       item.quality += 2;
     }
-    if (item.quality > 50) {
-      item.quality = 50;
-    }
+    this.keepQualityInBounds(item, this.LOWER_QUALITY_BOUND, this.UPPER_QUALITY_BOUND);
   }
-  updateBackstagePassesQuality(item: Item) {
+  private updateBackstagePassesQuality(item: Item) {
     if (item.sellIn <= 0) {
-      item.quality = 0;
+      item.quality = this.LOWER_QUALITY_BOUND;
     } else if (item.sellIn < 6) {
       item.quality += 3;
     } else if (item.sellIn < 11) {
       item.quality += 2;
     } else {
-      item.quality += 1;
+      item.quality++;
     }
-    if (item.quality > 50) {
-      item.quality = 50;
-    }
+    this.keepQualityInBounds(item, this.LOWER_QUALITY_BOUND, this.UPPER_QUALITY_BOUND);
   }
-  updateDefaultQuality(item: Item, multiplicator = 1) {
+  private updateDefaultQuality(item: Item, multiplicator = 1) {
     if (item.sellIn > 0) {
       item.quality -= 1 * multiplicator;
     } else {
       item.quality -= 2 * multiplicator;
     }
-    if (item.quality < 0) {
-      item.quality = 0;
+    this.keepQualityInBounds(item, this.LOWER_QUALITY_BOUND, this.UPPER_QUALITY_BOUND);
+  }
+  private keepQualityInBounds(item: Item, lowerBound = 0, upperBound = 50) {
+    if (item.quality < lowerBound) {
+      item.quality = lowerBound;
     }
-    if (item.quality > 50) {
-      item.quality = 50;
+    if (item.quality > upperBound) {
+      item.quality = upperBound;
     }
   }
 }
--- TypeScript/test/product-types/multiple-types.spec.ts
+++ TypeScript/test/product-types/multiple-types.spec.ts
@@ -1,61 +1,76 @@
 import { GildedRose, Item } from "@/gilded-rose";
-it.each([
-  [new Item('Standard Item', 10, 51)],
-  [new Item('Aged Brie', 2, 80)],
-  [new Item('Backstage passes to a TAFKAL80ETC concert', 15, 51)],
-  [new Item('Conjured Mana Cake', 1, 51)]
-])('should throw an error if quality exceeds 50',
-  (item) => {
-    const gildedRose = new GildedRose([item]);
-    expect(() => gildedRose.updateQuality()).toThrow('Quality cannot exceed 50');
-  });
 
-it.each([
-  [new Item('Standard Item', 10, -1)],
-  [new Item('Aged Brie', 2, -10)],
-  [new Item('Backstage passes to a TAFKAL80ETC concert', 15, -1)],
-  [new Item('Conjured', -3, -10)]
-])('should throw an error if quality drops below 0',
-  (item) => {
-    const gildedRose = new GildedRose([item]);
-    expect(() => gildedRose.updateQuality()).toThrow('Quality cannot be negative');
-  });
+describe('All items', () => {
 
-it.each([
-  [new Item('Sulfuras', 2, 80)],
-  [new Item('Sulfuras', -2, -10)]
-])('should not throw an error when Sulfuras exceeds quality boundaries of 0 and 50',
-  (item) => {
-    const gildedRose = new GildedRose([item]);
-    expect(() => gildedRose.updateQuality()).not.toThrow();
-  });
+  it.each([
+    [new Item('Standard Item', 10, 51)],
+    [new Item('Aged Brie', 2, 80)],
+    [new Item('Backstage passes to a TAFKAL80ETC concert', 15, 51)],
+    [new Item('Conjured Mana Cake', 1, 51)]
+  ])('should throw an error if quality exceeds 50',
+    (item) => {
+      const gildedRose = new GildedRose([item]);
+      expect(() => gildedRose.updateQuality()).toThrow(`Quality is out of bounds: ${item.quality}`);
+    });
 
-it.each([
-  [new Item('Aged Brie', -2, 49)],
-  [new Item('Aged Brie', 2, 50)],
-  [new Item('Backstage passes to a TAFKAL80ETC concert', 15, 50)],
-  [new Item('Backstage passes to a TAFKAL80ETC concert', 15, 49)],
-  [new Item('Backstage passes to a TAFKAL80ETC concert', 9, 50)],
-  [new Item('Backstage passes to a TAFKAL80ETC concert', 9, 49)],
-  [new Item('Backstage passes to a TAFKAL80ETC concert', 1, 50)],
-  [new Item('Backstage passes to a TAFKAL80ETC concert', 0, 49)]
-])('should not allow quality exceed 50',
-  (item) => {
-    const gildedRose = new GildedRose([item]);
-    gildedRose.updateQuality();
-    expect(gildedRose.items[0].quality).toBeLessThanOrEqual(50);
-  });
+  it.each([
+    [new Item('Standard Item', 10, -1)],
+    [new Item('Aged Brie', 2, -10)],
+    [new Item('Backstage passes to a TAFKAL80ETC concert', 15, -1)],
+    [new Item('Conjured', -3, -10)]
+  ])('should throw an error if quality drops below 0',
+    (item) => {
+      const gildedRose = new GildedRose([item]);
+      expect(() => gildedRose.updateQuality()).toThrow(`Quality is out of bounds: ${item.quality}`);
+    });
 
-it.each([
-  [new Item('Standard Item', -1, 0)],
-  [new Item('Backstage passes to a TAFKAL80ETC concert', -1, 0)],
-  [new Item('Conjured Mana Cake', -1, 0)],
-  [new Item('Conjured Mana Cake', 1, 0)],
-  [new Item('Conjured Mana Cake', -1, 3)],
-  [new Item('Conjured Mana Cake', 1, 1)]
-])('should not allow quality to drop below 0',
-  (item) => {
-    const gildedRose = new GildedRose([item]);
-    gildedRose.updateQuality();
-    expect(gildedRose.items[0].quality).toBeGreaterThanOrEqual(0);
-  });
+  it.each([
+    [new Item('Standard Item', 'abc', 50)],
+    [new Item('Aged Brie', 2, [])],
+    [new Item('Backstage passes to a TAFKAL80ETC concert', 15, 'test')],
+    [new Item('Conjured Mana Cake', {}, 5)]
+  ])('should throw an error if quality or sellIn is NaN',
+    (item) => {
+      const gildedRose = new GildedRose([item]);
+      expect(() => gildedRose.updateQuality()).toThrow(`Quality or sellIn is NaN`);
+    });
+
+  it.each([
+    [new Item('Sulfuras', 2, 80)],
+    [new Item('Sulfuras', -2, -10)]
+  ])('should not throw an error when Sulfuras exceeds quality boundaries of 0 and 50',
+    (item) => {
+      const gildedRose = new GildedRose([item]);
+      expect(() => gildedRose.updateQuality()).not.toThrow();
+    });
+
+  it.each([
+    [new Item('Aged Brie', -2, 49)],
+    [new Item('Aged Brie', 2, 50)],
+    [new Item('Backstage passes to a TAFKAL80ETC concert', 15, 50)],
+    [new Item('Backstage passes to a TAFKAL80ETC concert', 15, 49)],
+    [new Item('Backstage passes to a TAFKAL80ETC concert', 9, 50)],
+    [new Item('Backstage passes to a TAFKAL80ETC concert', 9, 49)],
+    [new Item('Backstage passes to a TAFKAL80ETC concert', 1, 50)],
+    [new Item('Backstage passes to a TAFKAL80ETC concert', 0, 49)]
+  ])('should not allow quality exceed 50',
+    (item) => {
+      const gildedRose = new GildedRose([item]);
+      gildedRose.updateQuality();
+      expect(gildedRose.items[0].quality).toBeLessThanOrEqual(50);
+    });
+
+  it.each([
+    [new Item('Standard Item', -1, 0)],
+    [new Item('Backstage passes to a TAFKAL80ETC concert', -1, 0)],
+    [new Item('Conjured Mana Cake', -1, 0)],
+    [new Item('Conjured Mana Cake', 1, 0)],
+    [new Item('Conjured Mana Cake', -1, 3)],
+    [new Item('Conjured Mana Cake', 1, 1)]
+  ])('should not allow quality to drop below 0',
+    (item) => {
+      const gildedRose = new GildedRose([item]);
+      gildedRose.updateQuality();
+      expect(gildedRose.items[0].quality).toBeGreaterThanOrEqual(0);
+    });
+})

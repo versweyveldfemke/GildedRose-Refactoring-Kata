39510250c4dfdcff8fd9d8b686d183e528fd2fe9
Femke Versweyveld <versweyveldfemke@gmail.com>
2025-08-03 10:53:12 +0200

    [Refactoring] Refactor existing code v2 - improve readability

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -38,91 +38,52 @@ export class GildedRose {
 
         switch (true) {
           case item.name.includes('Aged Brie'):
-            if (item.sellIn > 0) {
-              item.quality = item.quality + 1;
-            }
-            if (item.sellIn <= 0) {
-              item.quality = item.quality + 2;
-            }
+            this.updateAgedBrieQuality(item);
             break;
           case item.name.includes('Backstage passes'):
-            if (item.sellIn <= 0) {
-              item.quality = 0;
-            } else if (item.sellIn < 6) {
-              item.quality = item.quality + 3;
-            } else if (item.sellIn < 11) {
-              item.quality = item.quality + 2;
-            } else {
-              item.quality = item.quality + 1;
-            }
-
+            this.updateBackstagePassesQuality(item);
             break;
           default:
-            if (item.sellIn >= 0 && item.quality > 0) {
-              item.quality = item.quality - 1;
-            }
-            if (item.sellIn < 0 && item.quality > 1) {
-              item.quality = item.quality - 2;
-            }
+            this.updateDefaultQuality(item);
             break;
         }
         item.sellIn = item.sellIn - 1;
-
-        // Can only do step below because we assured quality is within bounds at the start
-        if (item.quality > 50) {
-          item.quality = 50;
-        }
-        if (item.quality < 0) {
-          item.quality = 0;
-        }
       });
 
-
-
-
-    // for (let i = 0; i < this.items.length; i++) {
-    //   if (
-    //     this.items[i].name != 'Aged Brie'
-    //     && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert'
-    //     && this.items[i].name != 'Sulfuras, Hand of Ragnaros'
-    //     && this.items[i].quality > 0
-    //   ) {
-    //     this.items[i].quality = this.items[i].quality - 1
-    //   } else {
-    //     if (this.items[i].quality < 50) {
-    //       this.items[i].quality = this.items[i].quality + 1
-    //       if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-    //         if (this.items[i].sellIn < 11 && this.items[i].quality < 50) {
-    //           this.items[i].quality = this.items[i].quality + 1
-    //         }
-    //         if (this.items[i].sellIn < 6 && this.items[i].quality < 50) {
-    //           this.items[i].quality = this.items[i].quality + 1
-    //         }
-    //       }
-    //     }
-    //   }
-    //   if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-    //     this.items[i].sellIn = this.items[i].sellIn - 1;
-    //   }
-    //   if (this.items[i].sellIn < 0) {
-    //     if (this.items[i].name != 'Aged Brie') {
-    //       if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-    //         if (this.items[i].quality > 0) {
-    //           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-    //             this.items[i].quality = this.items[i].quality - 1
-    //           }
-    //         }
-    //       } else {
-    //         this.items[i].quality = this.items[i].quality - this.items[i].quality
-    //       }
-    //     } else {
-    //       if (this.items[i].quality < 50) {
-    //         this.items[i].quality = this.items[i].quality + 1
-    //       }
-    //     }
-    //   }
-    // }
-
     return this.items;
+  }
+  updateAgedBrieQuality(item: Item) {
+    if (item.sellIn > 0) {
+      item.quality += 1;
+    } else {
+      item.quality += 2;
+    }
+    if (item.quality > 50) {
+      item.quality = 50;
+    }
+  }
+  updateBackstagePassesQuality(item: Item) {
+    if (item.sellIn <= 0) {
+      item.quality = 0;
+    } else if (item.sellIn < 6) {
+      item.quality += 3;
+    } else if (item.sellIn < 11) {
+      item.quality += 2;
+    } else {
+      item.quality += 1;
+    }
+    if (item.quality > 50) {
+      item.quality = 50;
+    }
+  }
+  updateDefaultQuality(item: Item) {
+    if (item.sellIn >= 0) {
+      item.quality -= 1;
+    } else {
+      item.quality -= 2;
+    }
+    if (item.quality < 0) {
+      item.quality = 0;
+    }
   }
 }
